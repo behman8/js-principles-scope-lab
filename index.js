4aa0232442f500fcf450dbcb039a17dc577fbe1a
Branden <brandenehman45@gmail.com>
2021-05-23 17:03:01 -0600

    commit tree d59a2d483ed2974813e0ba0605c71d12e1f89c6b

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 var customerName = 'bob';
+const leastFavoriteCustomer = 'Thanos';
 
-    function upperCaseCustomerName () {
-        return customerName.toUpperCase();
-    }
+function upperCaseCustomerName () {
+     customerName = customerName.toUpperCase();
+}
 
-    upperCaseCustomerName();
+var bestCustomer;
+function setBestCustomer () {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer () {
+    bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer () {
+    leastFavoriteCustomer = 'Spider-Man';
+}
